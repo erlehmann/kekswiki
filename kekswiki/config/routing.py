aecddc72245b02214a9171e787f30512b4e3ad41
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-11-01 18:28:48 +0100

    also, routing

--- kekswiki/config/routing.py
+++ kekswiki/config/routing.py
@@ -20,7 +20,7 @@ def make_map(config):
 
     # CUSTOM ROUTES HERE
 
-    map.connect('/{controller}/{action}')
-    map.connect('/{controller}/{action}/{id}')
+    map.connect('/:title', controller='article', action='index')
+    map.connect('/:title/:action', controller='article')
 
     return map
